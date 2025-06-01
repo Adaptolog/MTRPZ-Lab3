13e6decde8454d6194a08a0dae62b0af20be72e0
Adaptolog <yaroslawx@gmail.com>
2025-06-01 11:51:49 +0300

    Python task 5 -bullseye-
    -Added numpy to requirements
    -Implemented matrix-multiplication endpoint

--- Python/spaceship/routers/api.py
+++ Python/spaceship/routers/api.py
@@ -1,8 +1,18 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
-
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+@router.get("/matrix-multiplication")
+def multiply_matrices():
+    # Генеруємо дві матриці 10x10 з випадковими значеннями (0-1)
+    matrix_a = np.random.rand(10, 10).round(2).tolist()
+    matrix_b = np.random.rand(10, 10).round(2).tolist()
+    # Перемножуємо матриці
+    product = np.matmul(matrix_a, matrix_b).round(2).tolist()
+    
+    return {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product
+    }
